50cfe8b954c6eb579f9c9a98d258b77970c6ea6a
mvrikxix <akhil.sabu@integrtr.com>
2021-12-03 11:42:50 +0530

    setup: initial setup

---
+++ build/contentScript.4f89a7cf1783d97ed6ab.hot-update.js
@@ -0,0 +1,11 @@
+"use strict";
+self["webpackHotUpdatezoho_automate"]("contentScript",{},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("4ee2783688cfe3e4bd7a")
+/******/ })();
+/******/ 
+/******/ }
+);
+//# sourceMappingURL=contentScript.4f89a7cf1783d97ed6ab.hot-update.js.map
---
+++ src/pages/Options/Options.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Options = ({ title }) => {
+  return <div className="OptionsContainer">{title} Page</div>;
+};
+
+export default Options;
